bb0b74890cdf9a717292ec0bd3de50295f0e883d
qb-0 <saschap@mail.de>
2023-04-25 13:06:00 +0200

    .

--- compare_speed.py
+++ compare_speed.py
@@ -46,7 +46,14 @@ def measure_pymem(n):
         local_player.pos_y = proc.read_float(local + Addr.pos + 4)
         local_player.pos_z = proc.read_float(local + Addr.pos + 8)
     t_end = time()
-    print(f"pyMem (Local: {vars(local_player)}): Memory: {t_end - t_start} sec")
+    print(f"[R] pyMem (Local: {vars(local_player)}): Memory: {t_end - t_start} sec")
+    for _ in range(n):
+        proc.write_int(local + Addr.health, local_player.health + 1)
+        proc.write_float(local + Addr.pos, local_player.pos_x)
+        proc.write_float(local + Addr.pos + 4, local_player.pos_y)
+        proc.write_float(local + Addr.pos + 8, local_player.pos_z)
+    t_end2 = time()
+    print(f"[W] pyMem {t_end2 - t_end} sec")
     proc.close_process()
 
 
@@ -63,7 +70,14 @@ def measure_ctypes_mem(n):
         local_player.pos_y = proc.read(local + Addr.pos + 4, c_float())
         local_player.pos_z = proc.read(local + Addr.pos + 8, c_float())
     t_end = time()
-    print(f"cTypes Mem (Local: {vars(local_player)}): Memory: {t_end - t_start} sec")
+    print(f"[R] cTypes Mem (Local: {vars(local_player)}): Memory: {t_end - t_start} sec")
+    for _ in range(n):
+        proc.write(local + Addr.health, c_int(local_player.health + 1))
+        proc.write(local + Addr.pos, c_float(local_player.pos_x))
+        proc.write(local + Addr.pos + 4, c_float(local_player.pos_y))
+        proc.write(local + Addr.pos + 8 , c_float(local_player.pos_z))
+    t_end2 = time()
+    print(f"[W] cTypes Mem {t_end2 - t_end} sec")
     proc.close()
 
 
@@ -82,8 +96,14 @@ def measure_ctypes_mem_struct(n):
     local_player.pos_y = local_player_struct.pos_y
     local_player.pos_z = local_player_struct.pos_z
     t_end = time()
-    print(f"cTypes Mem Struct (Local: {vars(local_player)}): Memory: {t_end - t_start} sec")
+    print(f"[R] cTypes Mem Struct (Local: {vars(local_player)}): Memory: {t_end - t_start} sec")
+    local_player_struct.health = c_int(255)
+    for _ in range(n):
+        proc.write(local, local_player_struct)
+    t_end2 = time()
+    print(f"[W] cTypes Mem Struct {t_end2 - t_end} sec")
     proc.close()
+
 
 if __name__ == "__main__":
     its = 100000
